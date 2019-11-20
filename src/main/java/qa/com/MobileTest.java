d875b49c6c80df026dfdf8a128b11129d7bc4afa
egor-avksentyev <123456789QQww>
2019-11-20 14:51:33 +0200

    correction

--- src/main/java/qa/com/MobileTest.java
+++ src/main/java/qa/com/MobileTest.java
@@ -1,15 +1,16 @@
 package qa.com;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.ArrayList;
+
+
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
@@ -26,7 +27,7 @@ public class MobileTest extends storeMethods {
     public void setUp() {
 
         // If you want to disable infobars please use this code
-       
+
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
         options.setExperimentalOption("useAutomationExtension", false);
@@ -44,46 +45,50 @@ public class MobileTest extends storeMethods {
         driver.manage().window().maximize();
 
     }
+
     /**
      * Open Google page, search and quit
      */
-
     @Test
-
     public void mobileTest() {
-
         // Open Magazine
 
-       WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
 
         //Test1
-
+        System.out.println("Starting Tes1");
         driver.get("https://supsystic.com/example/comparison-example/");
-
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[contains(text(),'Samsung Galaxy S6')]/ancestor::div//span[contains(text(),'959')]"))));
-
+        /**
+         * getPrice() method uses current xpath , gets text from this xpath , replace "$" symbol in order to parse correctly and returns string which contains price.
+         * Please take into consideration that this method location is in the storeMethods class
+         *
+         * @param xpath current xpath
+         */
         String price1 = getPrice("//span[contains(text(),'Samsung Galaxy S6')]/ancestor::div//span[contains(text(),'959')]");
-
         System.out.println("Current Prise " + price1 + "$");
-
         String price2 = getPrice("//span[contains(text(),'Samsung Galaxy S6')]/ancestor::div//span[contains(text(),'2699')]");
-
-        System.out.println("The prise that was before "+ price2 + "$");
-
-        System.out.println("Feel the difference " + (Double.parseDouble (price2) - Double.parseDouble (price1)) + "$");
+        System.out.println("The prise that was before " + price2 + "$");
+        System.out.println("Feel the difference " + (Double.parseDouble(price2) - Double.parseDouble(price1)) + "$");
 
         //Test 2
-
+        System.out.println("Starting Tes2");
         openLinkinNewtab("https://unicode-table.com/ru/");
-
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//li[@data-code='81' and contains(text(),'Q')]"))));
-/**
- * */
-        System.out.println(xpathkiller(81,"Q"));
-        System.out.println(xpathkiller(38,"&"));
-        System.out.println(xpathkiller(65,"A"));
+
+        /**
+         * xpathkiller() Method with unique xpath
+         * Please take into consideration that this method location is in the storeMethods class
+         *
+         * @param number unique number
+         * @param letter proper letter
+         * */
+        System.out.println(xpathkiller(81, "Q"));
+        System.out.println(xpathkiller(38, "&"));
+        System.out.println(xpathkiller(65, "A"));
 
     }
+
     /**
      * After method, quit driver
      */
@@ -96,7 +101,6 @@ public class MobileTest extends storeMethods {
         // quit() - close all window an driver
         driver.quit();
     }
-
 
 
 }
--- src/main/java/qa/com/storeMethods.java
+++ src/main/java/qa/com/storeMethods.java
@@ -2,8 +2,6 @@ package qa.com;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.ArrayList;
 
@@ -20,11 +18,11 @@ public class storeMethods {
                 "^.*?(-?\\d+(\\.\\d+)?).*$", "$1");
     }
 
-    public void openLinkinNewtab (String link){
-    ((JavascriptExecutor)MobileTest.driver).executeScript("window.open()");
-    ArrayList<String> tabs = new ArrayList<String>(MobileTest.driver.getWindowHandles());
-    MobileTest.driver.switchTo().window(tabs.get(1));
-    MobileTest.driver.get(link);
+    public void openLinkinNewtab(String link) {
+        ((JavascriptExecutor) MobileTest.driver).executeScript("window.open()");
+        ArrayList<String> tabs = new ArrayList<String>(MobileTest.driver.getWindowHandles());
+        MobileTest.driver.switchTo().window(tabs.get(1));
+        MobileTest.driver.get(link);
     }
 
 }
